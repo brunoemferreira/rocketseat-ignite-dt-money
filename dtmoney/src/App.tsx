54e241e7a46ca5821f4b680debafcee88e272bb6
brunoemferreira <brunoemf@gmail.com>
2021-03-23 21:05:37 -0300

    creating global styles

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #f8f2f5;
+    --shape: #FFFFFF;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px (Desktop) é a padrão para Desktop
+  html{
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+    
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
+  body{
+    background: var(---background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+`
